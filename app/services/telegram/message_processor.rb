656682f41ed62de3a74d333be6fd376c20ed5af7
Loginov Vyacheslav <v.loginov@amoniac.eu>
2018-09-02 18:44:14 +0300

    Make assign possible

--- app/services/telegram/message_processor.rb
+++ app/services/telegram/message_processor.rb
@@ -16,7 +16,7 @@ module Telegram
         case command
         when 'help'
           @bot.api.sendMessage chat_id: chat_id,
-                               text: "[assign_to_room, create_room, room_list, account_link]"
+                               text: "[assign_to_room, create_room, room_list, account_link, current_room, i_am]"
         when 'create_room'
           room = Room.find_or_create_by name: attribute
           @bot.api.sendMessage chat_id: chat_id,
@@ -24,15 +24,28 @@ module Telegram
         when 'assign_to_room'
           room = Room.find_or_create_by name: attribute
           groups_chat = GroupsChat.find_or_create_by service: 'telegram',
-                                                     service_id: chat_id,
-                                                     room_id: room.id
+                                                     service_id: chat_id
 
           groups_chat.update! room_id: room.id
           @bot.api.sendMessage chat_id: chat_id,
                                text: "#{groups_chat.inspect}"
         when 'room_list'
           @bot.api.sendMessage chat_id: chat_id,
-                               text: Room.all.to_json
+                               text: Room.pluck(:name).to_json
+        when 'current_room'
+          groups_chat = GroupsChat.find_or_create_by service: 'telegram',
+                                                     service_id: chat_id
+
+          @bot.api.sendMessage chat_id: chat_id,
+                               text: groups_chat.to_json
+        when 'i_am'
+          user = User.find_by(email: attribute)
+          return unless user
+
+          messenger = create_messenger
+          messenger.update!(user_id: user.id)
+          @bot.api.sendMessage chat_id: chat_id,
+                               text: messenger.to_json
         end
       else
         create_message
@@ -48,14 +61,18 @@ module Telegram
 
     def create_message
       messenger = create_messenger
-      #user = User.find(messenger.user_id)
-      user = User.first
       groups_chat = create_groups_chat
-      groups_chat.room_id = 1
 
-      ::Message.create! sender_id: user.id,
-                        room_id:   groups_chat.room_id,
-                        body:      @message.text
+      if groups_chat.room_id
+        ::Message.create! sender_id: messenger.id,
+                          room_id:   groups_chat.room_id,
+                          body:      @message.text
+      else
+        bot_name = "@#{ENV['BOT_NAME']}"
+        text = "First, type #{bot_name} room_list\nThen, type #{bot_name} assign_to_room ROOM_NAME"
+        @bot.api.sendMessage chat_id: @message.chat.id,
+                             text: text
+      end
     end
 
     def create_groups_chat
